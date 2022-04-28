a922704cb1f676a46faf67582ea1c2bec818ef74
Aiello Chan <aiello.chan@gmail.com>
2022-04-28 15:54:30 +0800

    fix(parser): use babel instead of acorn.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const acorn = require("acorn");
-const walk = require("acorn-walk");
+const parser = require("@babel/parser");
 const loaderUtils = require("loader-utils");
+const { default: traverse } = require("@babel/traverse");
 
 /**
  * 根据相对路径和当前 loader 上下文解析出文件的绝对路径
+ *
+ * Resolve absolute path according to the relative path and the current loader context
  * @param {string} src
  * @param {webpack.loader.LoaderContext} context
  * @returns
@@ -19,31 +21,44 @@ const resolvePath = (src, context) =>
  * 获得当前源码中所有被 import 的值
  * 如: import a from './test.ts'
  * 会得到: ['./test.ts']
+ *
+ * Get all imported values ​​in the current source code
+ * e.g. import a from './test.ts'
+ * Will get: ['./test.ts']
  * @param {string} source
  * @returns
  */
 const getImportValues = (source) => {
   const result = [];
-  const ast = acorn.parse(source, {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  });
-  const walkConfig = {
+  let ast;
+  try {
+    ast = parser.parse(source, {
+      sourceType: "module",
+      plugins: ["jsx", "typescript"],
+    });
+  } catch (e) {
+    console.error("babel parse source error: ", source, e);
+    return result;
+  }
+  traverse(ast, {
     ImportDeclaration(node) {
-      result.push(node.source.value);
+      result.push(node.node.source.value);
     },
-  };
-  walk.simple(ast, walkConfig);
+  });
 
   return result;
 };
 
 function WebpackRestrictImportLoader(source) {
+  if (this.resourcePath.indexOf("node_modules") !== -1) {
+    return source;
+  }
+
   const callback = this.async();
   const importer = this.resourcePath;
-  const options = loaderUtils.getOptions(this) || { rules: [] };
+  const { rules } = loaderUtils.getOptions(this) || { rules: [] };
 
-  const jobs = options.rules.map(async (rule) => {
+  const jobs = rules.map(async (rule) => {
     const { from, to, run } = rule;
 
     if (from.test(importer)) {
